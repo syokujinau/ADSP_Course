1767a9cffb98b81bfa20a5c1617f33bcc3a883e8
syokujinau <npr286rx@rocketmail.com>
2019-11-04 16:44:32 +0800

    sine gen

--- sinwave_gen.m
+++ sinwave_gen.m
@@ -3,12 +3,12 @@ f2 = 15000;
 Amp = 1;
 T = 20;
  
-ts = 1/100;
+ts = 1/(1.5*f2);
 
 t = 0:ts:T;
 y = Amp*sin(2*pi*f1*t) + Amp*sin(2*pi*f2*t);
 
 csvwrite('_1khz_15khz_signal.txt', y);
 
-plot(t, y)
+# plot(t, y)
 pause

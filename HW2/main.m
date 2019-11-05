abb919c961dead4bcb4bb937e8cd6fa025eb2405
syokujinau <npr286rx@rocketmail.com>
2019-11-05 23:25:32 +0800

    recSTFT

--- HW2/main.m
+++ HW2/main.m
@@ -1,25 +1,25 @@
-dt=0.05;
-df=0.05;
+dt = 0.05;
+df = 0.05;
 
-t1=[0:dt:10-dt]; 
-t2=[10:dt:20-dt]; 
-t3=[20:dt:30];
+t1 = [0:dt:10-dt]; 
+t2 = [10:dt:20-dt]; 
+t3 = [20:dt:30];
 
-t=[0:dt:30];
-f=[-5:df:5]; 
+t = [0:dt:30];
+f = [-5:df:5]; 
 
-x=[cos(2*pi*t1), cos(6*pi*t2), cos(4*pi*t3)];
-B=1;
+x = [cos(2*pi*t1), cos(6*pi*t2), cos(4*pi*t3)];
+B = 1;
 
 tic
-y=recSTFT(x,t,f,B);
+y = recSTFT(x, t, f, B);
 toc
 
 mesh(abs(y))
 mesh(t,f,abs(y))
 colormap(gray(256))
-set(gca,'Ydir','normal')
-set(gca,'Fontsize',12)
-xlabel('Time(Sec)','Fontsize',12)
-ylabel('Frequency(Hz)','Fontsize',12)
-title('STFT of x(t)','Fontsize',12)
+set(gca, 'Ydir', 'normal')
+set(gca, 'Fontsize', 12)
+xlabel('Time(Sec)', 'Fontsize', 12)
+ylabel('Frequency(Hz)', 'Fontsize', 12)
+title('STFT of x(t)', 'Fontsize', 12)
